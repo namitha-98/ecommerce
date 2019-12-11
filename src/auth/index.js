92f727ba6bb38c093aad13caf01a32f99fef259a
namitha-98 <58780602+namitha-98@users.noreply.github.com>
2019-12-11 23:33:14 +0400

    Add files via upload

---
+++ src/auth/index.js
@@ -0,0 +1,71 @@
+import { API } from "../config";
+import { encrypt }from "../crypto";
+
+export const signup = user => {
+    console.log(user);
+    const encrypteduser = encrypt(user.name);
+    console.log(encrypteduser);
+    return fetch(`${API}/signup`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(encrypteduser)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const signin = user => {
+    return fetch(`${API}/signin`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const authenticate = (data, next) => {
+    if (typeof window !== "undefined") {
+        localStorage.setItem("jwt", JSON.stringify(data));
+        next();
+    }
+};
+
+export const signout = next => {
+    if (typeof window !== "undefined") {
+        localStorage.removeItem("jwt");
+        next();
+        return fetch(`${API}/signout`, {
+            method: "GET"
+        })
+            .then(response => {
+                console.log("signout", response);
+            })
+            .catch(err => console.log(err));
+    }
+};
+
+export const isAuthenticated = () => {
+    if (typeof window == "undefined") {
+        return false;
+    }
+    if (localStorage.getItem("jwt")) {
+        return JSON.parse(localStorage.getItem("jwt"));
+    } else {
+        return false;
+    }
+};
